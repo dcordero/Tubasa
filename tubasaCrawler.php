87fc86b614d138f3633f2b54f62e5df76f875a25
David Cordero <dcorderoramirez@gmail.com>
2017-04-15 01:37:29 +0200

    Add Crawler

---
+++ tubasaCrawler.php
@@ -0,0 +1,67 @@
+<?php
+header('Content-type: application/json');
+
+function request_url_contents($url) {
+  $tries = 0;
+  do {
+    if ($tries > 0) sleep(1); # Wait for a sec before retrieving again
+      $contents = file_get_contents($url);
+    $tries++;
+  }
+  while ($tries <= 5 && $contents === FALSE);
+  return $contents;
+}
+
+function crawlStops($lineId, $stops) {
+  $crawledStops = array();
+  $numberOfStops = count ($stops[0]);
+  for ($j=0; $j < $numberOfStops; $j++) {
+    $currentStop["idp"] = $stops[1][$j];
+    $currentStop["ido"] = $stops[2][$j];
+
+    $url = 'http://badajoz.twa.es/code/getparadas.php?idl=' . urlencode($lineId) . '&idp=' . urlencode($currentStop["idp"]) . '&ido=' . urlencode($currentStop["ido"]);
+    $scheduleTimeRaw = request_url_contents($url);
+
+    preg_match_all('/Parada:\W(.*?)<\/h3>/', $scheduleTimeRaw, $scheduleTime);
+
+    mb_internal_encoding('UTF-8');
+    $currentStop["label"] = (ucwords(mb_strtolower($scheduleTime[1][0])));
+
+    array_push ($crawledStops, $currentStop);
+  }
+  return $crawledStops;
+}
+
+$staticColor = array("FF3B30", "34AADC", "4CD964", "FF4981", "C644FC", "FF9500", "FFDB4C", "FF4981", "4A4A4A", "FF3A2D", "1F1F21", "C86EDF");
+
+$url = 'http://badajoz.twa.es/code/getlineas.php';
+$linesRaw = request_url_contents($url);
+preg_match_all('/mostrarParadas\(\'(L_.*?)\'\).*LINEA\W(.*?)<\/a>/mi', $linesRaw, $lines);
+
+$numberOfLines = count ($lines[0]);
+$result["lines"] = array();
+
+for ($i=0; $i < $numberOfLines; $i++) {
+  $lineId = $lines[1][$i];
+  $lineName = $lines[2][$i];
+
+  $currentLine["code"] = $lineId;
+  $currentLine["label"] = $lineName;
+  $currentLine["color"] = $staticColor[$i];
+
+  $url = 'http://badajoz.twa.es/code/getparadas.php?idl=' . urlencode($lineId);
+  $stopsRaw = request_url_contents($url);
+
+  preg_match_all('/id="ar0-.*?value,\W\'(.*?)::(\d+.\d+)\'/', $stopsRaw, $stops);
+  $currentLine["stops"]["outbound"] = crawlStops($lineId, $stops);
+
+
+  preg_match_all('/id="ar1-.*?value,\W\'(.*?)::(\d+.\d+)\'/', $stopsRaw, $stops);
+  $currentLine["stops"]["return"] = array_reverse(crawlStops($lineId, $stops));
+
+  array_push ($result["lines"], $currentLine);
+}
+
+echo json_encode($result, JSON_UNESCAPED_UNICODE);
+?>
+
